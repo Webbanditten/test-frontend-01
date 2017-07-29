6c6e70337362f74a2aad778e47489fe92134add0
Patrick Nærvig Wøhlk <patrick-wohlk@hotmail.com>
2017-07-29 13:09:53 +0200

    Added homegrown boilerplate

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About me</h1>
+                <p>Im awesome thats whats this is about!</p>
+            </div>
+        );
+    }
+}
+
+
+export default AboutPage;
